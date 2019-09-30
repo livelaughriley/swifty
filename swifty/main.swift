8b6e22b80c90d5c391cb153e483793db4e2d7fe0
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-09-30 16:17:25 -0500

    Initial Commit

---
+++ swifty/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swifty
+//
+//  Created by Riley Roach on 9/30/19.
+//  Copyright © 2019 Riley Roach. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
