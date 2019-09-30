f114557cb53abcf9376ba7cbed984841ab5a1b7e
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-09-30 17:08:13 -0500

    testing project configuration

--- swifty/main.swift
+++ swifty/main.swift
@@ -6,7 +6,20 @@
 //  Copyright © 2019 Riley Roach. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
 
+import SwiftCLI
+
+class GreetCommand: Command {
+    let name = "greet"
+    let person = Parameter()
+    func execute() throws {
+        stdout <<< "Hello \(person.value)!"
+    }
+}
+
+let greeter = CLI(name: "greeter")
+greeter.commands = [GreetCommand()]
+greeter.go()
