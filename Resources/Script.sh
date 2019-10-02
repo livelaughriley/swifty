f0d46027c6350886aae3de6fbe923000e1fcae03
rileytwo <34988536+rileytwo@users.noreply.github.com>
2019-10-02 18:38:58 -0500

    move Script.sh

---
+++ Resources/Script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#  Script.sh
+#  greeter
+#
+#  Created by Riley Roach on 10/2/19.
+#  Copyright © 2019 Riley Roach. All rights reserved.
+
+cd ${PROJECT_DIR}
+mkdir -vp release
+
+cd release
+    
+pkg_dir=${PRODUCT_NAME}-${CURRENT_PROJECT_VERSION}
+
+rm -rf ${pkg_dir}
+mkdir ${pkg_dir}
+
+#product="${BUILD_PRODUCTS_DIR}"/"${PRODUCT_NAME}"
+
+cp -rp ${BUILT_PRODUCTS_DIR}/greeter ${pkg_dir}
+
+chmod +x ${pkg_dir}
+open ${PROJECT_DIR}/release/${pkg_dir}
+
