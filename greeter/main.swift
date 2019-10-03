9c19adf8e222e9ba8ab7dd4c3947adc6e73fbf85
rileytwo <34988536+rileytwo@users.noreply.github.com>
2019-10-02 19:58:19 -0500

    get a working build

--- greeter/main.swift
+++ greeter/main.swift
@@ -1,6 +1,6 @@
 //
 //  main.swift
-//  swifty
+//  greeter
 //
 //  Created by Riley Roach on 9/30/19.
 //  Copyright © 2019 Riley Roach. All rights reserved.
@@ -13,13 +13,34 @@
 import SwiftCLI
 
 class GreetCommand: Command {
-    let name = "greet"
+    let name: String = "greet"
+    let shortDescription: String = "Say hello to <input>"
+    
     let person = Parameter()
+    
+    let loud = Flag("-l", "--loud", description: "Say the greeting loudly")
+    let quiet = Flag("-q", "--quiet", description: "Say the greeting quietly")
+    
     func execute() throws {
-        stdout <<< "Hello \(person.value)!"
+        let greeting = "Hello \(person.value)!"
+        
+        if loud.value {
+            stdout <<< greeting.uppercased()
+        }
+        else if quiet.value {
+            stdout <<< greeting.lowercased()
+        }
+        else {
+            stdout <<< greeting
+        }
     }
 }
 
-let greeter = CLI(name: "greeter")
+let greeter = CLI(
+    name: "greeter",
+    version: "0.0.1",
+    description: "Greeter - a friendly greeter"
+)
+
 greeter.commands = [GreetCommand()]
 greeter.go()
